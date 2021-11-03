24c61efa7bf7ecd8738ad8f70fda2337df3982f8
salvosanzone <salvosansone.ss@gmail.com>
2021-11-03 15:00:39 +0100

    ripristino totale

--- js/script.js
+++ js/script.js
@@ -6,42 +6,73 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
-
-//richiamo e inizializzo la classe, ossia il container, dove poi andrò a costruire i quadratini(square)
-const container =document.querySelector('.container');
-console.log('ho richiamato:', container);
-
-
-//costruisco gli square utilizzando un ciclo for (level:easy)
-for(let i = 0; i < 100; i++){
-  const sq = document.createElement('div');
-  sq.className = ('square-easy-level');
-  container.append(sq);
-  console.log('elemento creato', sq);
-}
-//costruisco gli square utilizzando un ciclo for (level:hard)
-// for(let i = 0; i < 81; i++){
-//   const sq = document.createElement('div');
-//   sq.className = ('square-hard-level');
-//   container.append(sq);
-//   console.log(sq);
-// }
-//costruisco gli square utilizzando un ciclo for (level:crazy)
-// for(let i = 0; i < 49; i++){
-//   const sq = document.createElement('div');
-//   sq.className = ('square-crazy-level');
-//   container.append(sq);
-//   console.log(sq);
-// }
-
-//richiamo il selettore (selector) e lo inizializzo
-const selector =document.getElementById('selector');
-
-//creo l'evento click
-selector.addEventListener('change', function(){
-  console.log('ho cliccato',selector);
-  const options = selector.querySelectorAll('option');
-  options = 
-
-  //devo inserire il ciclo for in riferimento al value scelto
+//aggancio il pulsante play tramite l'id poi al click farà partire la funzione 'game'
+document.getElementById('play').addEventListener('click',function(){
+  //inserisco la funzione
+  game();
 })
+
+
+//questa è la funzione collegata al pulsante
+function game(){
+
+  //aggancio la select con l' id,la inizializzo e poi aggiungo alla fine value in modo tale da mostrarmeli
+  const levelOfDifficulty = parseInt(document.getElementById('level').value);
+  //ricaviamo il numero di celle in base al livello 
+  let cellNumbers;
+  let cellsPerRow;
+  switch (levelOfDifficulty) {
+    case 1:
+      cellNumbers = 100; //numero di celle
+      cellsPerRow = 10; //numero di celle per riga
+      break;
+    case 2:
+      cellNumbers = 81;
+      cellsPerRow = 9;
+      break;
+    case 3:
+      cellNumbers = 49;
+      cellsPerRow = 7;
+      break;
+  }
+
+  console.log('numero celle:',cellNumbers);
+  console.log('numero celle per riga',cellsPerRow);
+  //prima attivare la funzione generatePlayGround do una stringa vuota cosi resetta il main
+  document.querySelector('main').innerHTML = '';
+  generatePlayGround();
+
+//adesso devo creare il mio container(il playground 500*500)
+
+function generatePlayGround(){
+  const container = document.createElement('div');
+  container.className = 'container'; //prendo la classe .container nel css
+  //celle
+  for (let i = 1; i <= cellNumbers; i++){
+    //creo ogni singolo square
+    const square = document.createElement('div');
+    square.className = 'square'; //prendo la classe .square nel css
+    square.innerHTML = `<span>${i}</span>`; //inserisco i numeri
+    //modifico la dimensione degli square in base al numero di celle per riga(cellsPerRow)
+    const cellSize = `calc(100% / ${cellsPerRow})`;
+    square.style.width = cellSize;
+    square.style.height = cellSize;
+    console.log(cellSize);
+
+
+
+   //appendo al container gli square
+   container.append(square);
+
+
+
+  }
+  //appendo al main il container
+  document.querySelector('main').append(container);
+
+}
+
+
+}
+
+ 
